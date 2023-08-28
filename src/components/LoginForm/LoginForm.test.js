97b4ef828657f87071a9be4999e163e067f8a695
unknown <bereketlemma4@gmail.com>
2023-08-28 10:07:17 +0300

    unit test passed

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,10 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import LoginForm from '.';
+const { validatePassword } = require("./index");
+import * as EmailValidator from "email-validator";
 
-test('renders sign in page', () => {
-  render(<LoginForm />);
-  const signInText = screen.getByText("Sign in");
-  expect(signInText).toBeInTheDocument();
+describe("LoginForm validator", () => {
+  // test case for password validation
+  it("should return success message if password is valid", () => {
+    const password = "1234@ABCdef";
+    const result = validatePassword(password);
+    expect(result).toEqual(true);
+  });
+
+  it("should return an error message if password is invalid", () => {
+    const password = "1234@1234";
+    const result = validatePassword(password);
+    expect(result).toEqual(false);
+  });
+
+  // Test case for email validation
+  it("should return success message if email is valid", () => {
+    const email = "bereketlemma@gmail.com";
+    const result = EmailValidator.validate(email);
+    expect(result).toEqual(true);
+  });
+
+  it("should return error message if email is invalid", () => {
+    const email = "b@gmail";
+    const result = EmailValidator.validate(email);
+    expect(result).toEqual(false);
+  });
 });
-
-// Add more unit test here
--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,41 +1,74 @@
-import { useState } from 'react';
-import Alert from '@mui/material/Alert';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Snackbar from '@mui/material/Snackbar';
-import Typography from '@mui/material/Typography';
-import logo from '../../assets/logo.svg';
+import { useState } from "react";
+import Alert from "@mui/material/Alert";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Snackbar from "@mui/material/Snackbar";
+import Typography from "@mui/material/Typography";
+//import logo from "../../assets/logo.svg";
+import FormHelperText from "@mui/material/FormHelperText";
 
+import * as EmailValidator from "email-validator";
 
-export default function LoginForm() {
+export const validatePassword = (password) => {
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^*?&])[A-Za-z\d@$!%^*?&]{8,}$/;
+
+  const checkPassword = passwordRegex.test(password);
+
+  return checkPassword;
+};
+
+const LoginForm = () => {
   const [showAlert, setShowAlert] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [emailIsValid, setEmailIsValid] = useState(true);
+  const [passwordIsValid, setPasswordIsValid] = useState(true);
+
   const validateForm = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const data = new FormData(event.currentTarget);
-    const email = data.get('email');
-    const password = data.get('password');
+    const email = data.get("email");
+    const password = data.get("password");
 
     // Add validation code here
 
-  }
+    // check email validity
+    const emailCheck = EmailValidator.validate(email);
+    setEmailIsValid(emailCheck);
+
+    // check password validity
+    const passwordCheck = validatePassword(password);
+    setPasswordIsValid(passwordCheck);
+
+    if (!emailCheck || !passwordCheck) {
+      return false;
+    }
+
+    return true;
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
-    validateForm(event);
-    setShowAlert("Login Successful");
+
+    if (validateForm(event)) {
+      setShowAlert("Login Successful");
+    }
   };
 
   return (
-    <>
-      {showAlert &&
+    <div>
+      {showAlert && (
         <Snackbar
           open={showAlert}
           autoHideDuration={6000}
@@ -44,19 +77,22 @@ export default function LoginForm() {
         >
           <Alert>{showAlert}</Alert>
         </Snackbar>
-      }
+      )}
+
       <Grid
         item
         xs={false}
         sm={4}
         md={7}
         sx={{
-          backgroundImage: 'url(https://source.unsplash.com/random)',
-          backgroundRepeat: 'no-repeat',
+          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundRepeat: "no-repeat",
           backgroundColor: (t) =>
-            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -64,31 +100,41 @@ export default function LoginForm() {
           sx={{
             my: 8,
             mx: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Box sx={{
-            my: 2
-          }}>
-            <img src={logo} width="147" alt="harrison.ai" />
+          <Box
+            sx={{
+              my: 2,
+            }}
+          >
+            {/*  <img src={logo} width="147" alt="harrison.ai" /> */}
           </Box>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
+          >
             <TextField
+              error={!emailIsValid}
               margin="normal"
               required
               fullWidth
-              id="email"
+              id="email "
               label="Email Address"
               name="email"
               autoComplete="email"
               autoFocus
+              helperText={!emailIsValid && "Enter a valid email"}
             />
             <TextField
+              error={!passwordIsValid}
               margin="normal"
               required
               fullWidth
@@ -97,6 +143,19 @@ export default function LoginForm() {
               type="password"
               id="password"
               autoComplete="current-password"
+              helperText={
+                !passwordIsValid && (
+                  <FormHelperText>
+                    <Typography variant="body2">
+                      - Minimum 8 characters
+                      <br />
+                      - Should contain both uppercase and lowercase letter
+                      <br />- Minimum of 1 numerical digit (0-9)
+                      <br />- Minimum of 1 special character (!@#$%^&*, etc)
+                    </Typography>
+                  </FormHelperText>
+                )
+              }
             />
             <Button
               type="submit"
@@ -109,6 +168,8 @@ export default function LoginForm() {
           </Box>
         </Box>
       </Grid>
-    </>
+    </div>
   );
-}
+};
+
+export default LoginForm;
